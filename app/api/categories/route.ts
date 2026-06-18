0056caaddd7f7afc5b32b959ccd2afa172ba1462
abhay0826R-dev <abhay0826r@gmail.com>
2026-06-18 20:24:48 +0530

    feat: GET categories

---
+++ app/api/categories/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request: Request) {
+    const categories = await prisma.category.findMany ({
+        select: {
+            count: true,
+            icon: true,
+            label: true,
+            tone: true,
+        }
+    });
+
+    return NextResponse.json(categories)
+}
