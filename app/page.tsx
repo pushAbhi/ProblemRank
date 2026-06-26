e5b1540246adc8da35637af82056f234b6039b3e
abhay0826R-dev <abhay0826r@gmail.com>
2026-06-26 20:12:37 +0530

    feat: google sign in/up

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import HeroBanner from "./components/HeroBanner";
 import Categories from "./components/Categories";
 import Footer from "./components/Footer"
 import Problems from "./components/Problems";
+import SignIn from "./components/sign-in";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-secondary">
       <Navbar />
       <HeroBanner />
-
+      <SignIn />
       <main className="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
         {/* Categories */}
         <Categories />
